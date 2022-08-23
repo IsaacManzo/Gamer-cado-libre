935224762eb6b65884b3ca1e94094b09a6a43170
Emmanuel <emmanuel.dacal@gmail.com>
2022-08-23 16:10:36 -0300

    sign in temporal

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,7 @@ import {Route, Routes, useNavigate} from 'react-router-dom'
 import Navbar from './commons/Navbar';
 import Footer from './commons/Footer';
 import SignUp from './components/Signup';
+import SignIn from './components/SIgnin';
 //import signIn from './components/signIn'
 
 function App() {
@@ -15,12 +16,13 @@ function App() {
           <div className="App">
             <Navbar/>
             <button onClick={()=>{navigate('/signup')}}>signup</button>
+            <button onClick={()=>{navigate('/signin')}}>signin</button>
             <Footer/>
           </div>
         }/>
         <Route path='/cart' element={''}/>
         <Route path='/signup' element={<><SignUp/></>}/>
-        {/* <Route path='/signin' element={<signIn/>}/> */}
+        <Route path='/signin' element={<SignIn/>}/>
         <Route path='/products' element={''}/>
         <Route path='/products/:genre' element={''}/>
       </Routes>
