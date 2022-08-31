3fa70900fe819d167c099a5f2315afb2cddb6ed5
IsaacManzo <isaacemanzo51@gmail.com>
2022-08-31 11:42:23 -0300

    m

--- front/src/components/admin_panel/Admin_users.jsx
+++ front/src/components/admin_panel/Admin_users.jsx
@@ -23,8 +23,68 @@ setUser(users.data)
   )
 }
 
-export function AllUsers({product}) {
+export function GridAllUsers({product}) {
 
+  const navigate = useNavigate()
+
+  const handleRemove = function (){
+      axios.delete(`http://localhost:3001/api/products/${product.id}`)
+      .then(message=>{
+          if(message.status == 204){
+              window.location.reload()
+              alert('Producto eliminado correctamente')
+          }
+      })
+  }
+
+  const handleEdit = function (){
+      navigate(`/product/${product.id}`)
+  }
+
+return (
+  <div className='paper'>
+      <Paper
+      sx={{
+          p: 2,
+          margin: 'auto',
+          maxWidth: 500,
+          flexGrow: 1,
+          backgroundColor: (theme) =>
+          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+      }}
+      >
+      <Grid container spacing={2}>
+          <Grid item>
+          <ButtonBase sx={{ width: 128, height: 128 }}>
+              <Img alt="complex" src={`https://images.igdb.com/igdb/image/upload/t_cover_small/${product.urlId}.jpg`} />
+          </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+          <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+              <Typography gutterBottom variant="subtitle1" component="div">
+                  {product.name}
+              </Typography>
+              </Grid>
+              <Grid item>
+              <Button sx={{ cursor: 'pointer' }} variant="body2" onClick={handleEdit}>
+                  Edit
+              </Button>
+              <Button sx={{ cursor: 'pointer' }} variant="body2" onClick={handleRemove}>
+                  Remove
+              </Button>
+              </Grid>
+          </Grid>
+          <Grid item>
+              <Typography variant="subtitle1" component="div">
+              ${product.price}
+              </Typography>
+          </Grid>
+          </Grid>
+      </Grid>
+      </Paper>
+  </div>
+);
 }
 
 
